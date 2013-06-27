f824927507cecb851f6f31681dbb128fb1f73080
Joshua Noble <joshuajnoble@gmail.com>
2013-06-27 15:47:49 -0700

    timing problems

--- cityGameTileV2/cityGameTileV2.ino
+++ cityGameTileV2/cityGameTileV2.ino
@@ -1,4 +1,6 @@
 
+#define __AVR_ATtinyX4__
+
 #include <MANCHESTER.h>
 #include <SoftwareSerial.h>
 
@@ -7,7 +9,7 @@ SoftwareSerial TinySerial(2,3); // RX, TX
 // what edges are we reading pins on 
 const int READ_PIN = 7;
 // edge detect
-const int THRESHOLD = 15;
+const int THRESHOLD = 800;
 
 unsigned int counter = 0;
 
@@ -17,33 +19,41 @@ const int CONNECTING = 1;
 const int CONNECTED = 2;
 int mode = WAITING;
 
-const int ID = '3';
+const int ID = 2;
 int lastConnectedSide = -1;
 
 const int sideConnections[6] = {0, 1, 4, 5, 6, 7};
 const int connectionToSideMap[6] = {0, 1, 2, 3, 4, 5};
 
+const long sendIDInterval = 50;
+long sendIDAgain = 0;
+
 void setup()  
 {
   // Open serial communications and let us know we are connected
   TinySerial.begin(1200);
-  pinMode(1, OUTPUT);
-  pinMode(READ_PIN, INPUT);
+  pinMode(10, OUTPUT);
+  int i = 0;
+  while( i < 6) {
+    pinMode(sideConnections[i], INPUT);
+    i++;
+  }
+  
   MANCHESTER.SetTxPin(0);
+  
 }
 
 void loop()
 {
 
   if( mode == CONNECTING ) {
+    
+    //digitalWrite(10, HIGH);
+    blink(10, 1);
 
     if(TinySerial.available())
     {
       int received = TinySerial.parseInt();
-      if(received > 0 && received < 64) // how many tiles will we have?
-      {    
-        Blink(1, received); //debug confirmation
-      }
 
       TinySerial.flush();
 
@@ -59,17 +69,24 @@ void loop()
         delay(100); // wait for Serial to finish up
         sendConnection(ID, received, lastConnectedSide);
         mode = CONNECTED;
+        blink(10, 3);
       } 
       else if( received > ID ) 
       {
         mode = CONNECTED;
+        blink(10, 2);
       }
 
     }
     else
     {
-      TinySerial.print(ID);
-      mode = CONNECTING;
+      
+      if(millis() - sendIDAgain > sendIDInterval)
+      {
+        TinySerial.print(ID);
+        sendIDAgain = millis();
+      }
+      
     }
   } 
 
@@ -79,29 +96,39 @@ void loop()
     if(analogRead(sideConnections[lastConnectedSide]) < THRESHOLD) // we just lost our connection
     {
       lastConnectedSide = -1;
+      // need to send here too
       mode = WAITING;
     }
   }
 
   if(mode == WAITING)
   {
-    // two cases: we're not connected yet:
     if(lastConnectedSide == -1 )
     {
       int i = 0;
-      while( i < 6) {
+      
+      /*while( i < 6) {
         if(analogRead(sideConnections[i]) > THRESHOLD)
         {
           lastConnectedSide = connectionToSideMap[i];
           mode = CONNECTING;
+          return;
         }
         i++;
+      }*/
+      
+      if(analogRead(7) > THRESHOLD)
+      {
+        lastConnectedSide = connectionToSideMap[i];
+        mode = CONNECTING;
+        return;
       }
+      
     }
   }
 }
 
-void Blink(byte led, byte times){ // poor man's display
+void blink(byte led, byte times){ // poor man's display
   for (byte i=0; i< times; i++){
     digitalWrite(led,HIGH);
     delay (250);
